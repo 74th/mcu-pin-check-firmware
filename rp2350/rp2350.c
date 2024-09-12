57a02d2f1566da7126f2d58e8dfc6b213c112c09
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2024-09-12 22:42:49 +0900

    rp2350

--- rp2350/rp2350.c
+++ rp2350/rp2350.c
@@ -1,22 +1,39 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-#define LED_PIN PICO_DEFAULT_LED_PIN
-// #define LED_PIN 18
+#define GPIO_NUM 48
 
 int main()
 {
     stdio_init_all();
+    bool led_state[GPIO_NUM] = {0};
 
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    for (uint i = 0; i < GPIO_NUM; i++)
+    {
+        gpio_init(i);
+        gpio_set_dir(i, GPIO_IN);
+        gpio_pull_up(i);
+    }
+
+    sleep_ms(10);
+
+    for (uint i = 0; i < GPIO_NUM; i++)
+    {
+        led_state[i] = gpio_get(i);
+    }
+
+    printf("init\n");
 
     while (true)
     {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-        gpio_put(LED_PIN, 1);
-        sleep_ms(1000);
-        gpio_put(LED_PIN, 0);
+        sleep_ms(100);
+        for (uint i = 0; i < GPIO_NUM; i++)
+        {
+            if (led_state[i] != gpio_get(i))
+            {
+                led_state[i] = !led_state[i];
+                printf("GPIO %d: %s\n", i, led_state[i] ? "H" : "L");
+            }
+        }
     }
 }
